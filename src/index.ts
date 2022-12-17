e1f612b5a97e2e25020037ff547a33f3c3a5697d
Mikko Ilmari Nummelin <mnummeli@gmail.com>
2022-12-17 18:02:22 +0200

    w3.css ym.

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,36 @@
 'use strict';
 
 const os = require('node:os');
+const path = require('node:path');
 import express, { Express, Request, Response } from 'express';
 const app: Express = express();
 const PORT= process.env.port || 3000;
 
+interface Model {
+    "title": string,
+    "visitCount": string | number,
+    "hostname": string,
+    "timeString": string
+}
+
 app.set('view engine', 'pug');
 
 app.get('/', (req: Request, res: Response) => {
-    const model = {
+    const model: Model = {
         "title": "Tietoa",
-        "visitCount": 0,
-        "hostname": os.hostname()
+        "visitCount": "Ei yhteyttä Redis-palvelimeen.",
+        "hostname": os.hostname(),
+        "timeString": "Kellonaika ei tiedossa."
     };
+    const currentTime: Date = new Date();
+    const hours = currentTime.getHours().toString().padStart(2, '0');
+    const minutes = currentTime.getMinutes().toString().padStart(2, '0');
+    const seconds = currentTime.getSeconds().toString().padStart(2, '0');
+    model.timeString = `${hours}:${minutes}:${seconds}`;
     res.render('index', model);
 });
+
+app.use(express.static(path.join(__dirname, 'static')))
 
 app.listen(PORT, () => {
     console.log(`Sovellus vastaa portissa: ${PORT}.`);
